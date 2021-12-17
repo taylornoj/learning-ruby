305b647794f4e8c543129c873592738af66b5bd6
Taylor Noj <taylornoj@gmail.com>
2021-12-17 18:08:06 +0000

    complete example 13

---
+++ ex13.rb
@@ -0,0 +1,9 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# run file:
+# ruby ex13.rb apple orange grapefruit
+
