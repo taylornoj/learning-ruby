54474e1a1964f5c684c374a093bbae135e864b46
Taylor Noj <taylornoj@gmail.com>
2021-12-17 11:46:35 +0000

    complete example 6

--- ex6.rb
+++ ex6.rb
@@ -1,21 +1,40 @@
+# variable
 types_of_people = 10
+
+# assigns string to variable
 x = "There are #{types_of_people} types of people."
+
+#variable
 binary = "binary"
+
+# variable
 do_not = "don't"
+
+# assigns string to variable and contains two more strings
 y = "Those who know #{binary} and those who #{do_not}."
 
+#printed
 puts x
+#printed
 puts y
 
+# printed
 puts "I said: #{x}."
+
+# printed
 puts "I also said: '#{y}'."
 
+# variable
 hilarious = false
+
 joke_evaluation = "Isn't that job so funny?! #{hilarious}"
 
+# printed
 puts joke_evaluation
 
+# assigning strings to variables
 w = "This is the left side of ..."
 e = "a string with a right side."
 
+# printed side by side
 puts w + e
