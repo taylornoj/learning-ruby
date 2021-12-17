d02073cfc415fea656d730060d5991c12939743d
Taylor Noj <taylornoj@gmail.com>
2021-12-17 22:55:15 +0000

    complete example 34

---
+++ ex34.rb
@@ -0,0 +1,27 @@
+# ACCESSING ELEMENTS OF ARRAYS
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# animal at 1
+puts animals[1]
+
+# 3rd animal
+puts animals[2]
+
+# 1st animal
+puts animals[0]
+
+# animal at 3
+puts animals[3]
+
+# 5th anmial
+puts animals[4]
+
+# animal at 2
+puts animals[2]
+
+# 6th animal
+puts animals[5]
+
+# animal at 4
+puts animals[4]
