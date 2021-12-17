ec741fbabb97d1364c3fe2f946c075916164034e
Taylor Noj <taylornoj@gmail.com>
2021-12-17 11:58:12 +0000

    complete example 11

---
+++ ex11.rb
@@ -0,0 +1,11 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weight? "
+weight = gets.chomp
+# the response is a string
+# gets.chomp has been assigned to a variable so we can re use it
+
+# prints string with response variables inside
+puts "So, you're #{age} old #{height} tall and #{weight} heavy."
