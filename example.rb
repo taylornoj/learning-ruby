c984e3f21b7612afdc8e9c52cae995f84368c72c
Taylor Noj <taylornoj@gmail.com>
2021-12-17 12:20:57 +0000

    updated notes

--- example.rb
+++ example.rb
@@ -1,3 +1,8 @@
 puts "Hello, what's your name?";
 name = gets.chomp;
 puts "Welcome to \"Ruby land\", #{name}. Hope you enjoy your stay (and never leave!)"
+
+
+# print vs. puts
+# print keeps response on the same line
+# puts means a new line /n is created
